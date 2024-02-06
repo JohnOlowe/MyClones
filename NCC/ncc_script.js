9b93331f3c588264cbc9ad07663e9311c8e8b979
JohnOlowe <olowejohn.2006@gmail.com>
2024-02-06 23:30:00 +0100

    Added icons and description

--- NCC/ncc_script.js
+++ NCC/ncc_script.js
@@ -11,3 +11,33 @@ function showMore() {
     button.innerHTML = "More...";
   }
 }
+
+function getText() {
+  const textField = document.querySelector("form .text").value;
+  if (textField.trim().length == 0) {
+    const errorField = document.querySelector(".comments span.empty_field_error");
+    errorField.style.display = "block";
+    errorField.innerHTML = "Enter a value into the comment field.";
+    return;
+  }
+  const commentTemplate = `
+    <div class="comment_item">
+      <span class="comment_time">${getTime()}</span>
+      <p class="comment_text">${textField}</p>
+    </div>
+  `;
+  document.querySelector("form .text").value = "";
+  document.querySelector(".comments_container").innerHTML += commentTemplate;
+}
+
+function getTime() {
+  const date = new Date();
+  return pad(date.getDate()) + "/" + pad(date.getMonth() + 1) + "/" + date.getFullYear() + " " + pad(date.getHours()) + ":" + pad(date.getMinutes());
+}
+
+function pad(value) {
+  if (value < 10) {
+    return "0" + value;
+  }
+  return value;
+}
